c7b00db2dc6da4942287d3538b917d0885ecb4a9
Ahmed Yousri <ahmedyousri680@outlook.com>
2024-09-07 16:34:17 +0300

    [FIX] CORS Issue

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 import { dbConnection } from './db/connection.js';
 import userRoutes from './src/modules/users/user.routes.js';
-import cors from 'cors'
-const app = express()
+
+const app = express();
+
+// Enable CORS for the React frontend
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow React's port 3000
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  credentials: true, // Enable cookies and other credentials
+}));
+
 const port = 5000;
 dotenv.config({})
 app.use(express.json());
@@ -11,10 +20,6 @@ app.use(express.json());
 dbConnection()
 app.use('/user',userRoutes)
 // app.use(cors({origin: "http://localhost:3000/register"}))
-app.use(cors({
-    origin: 'http://localhost:3000/register', 
-    credentials: true
-  }));
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
